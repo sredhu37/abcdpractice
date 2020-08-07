52d94fbdcb1951687853b9b4c971213690a4894f
i342082 <sunny.redhu@sap.com>
2020-08-07 17:58:40 +0530

    Change readin env vars

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,76 +4,48 @@ const { exists } = require('./commonMethods');
 
 const resultObj = {};
 
-const NODE_ENV = exists(process.env.NODE_ENV) ? process.env.NODE_ENV : 'PROD';
+if (exists(process.env.MONGO_URI)) {
+  const mongoObj = { URI: process.env.MONGO_URI };
+  resultObj.mongo = mongoObj;
+} else {
+  logger.error('Issue with MONGO env vars! Please inform Administrator immediately!');
+  process.exit(1);
+}
 
-if (NODE_ENV === 'DEV') {
-  if (exists(process.env.MONGO_URI_DEV)) {
-    const mongoObj = { URI: process.env.MONGO_URI_DEV };
-    resultObj.mongo = mongoObj;
-  } else {
-    logger.error('Issue with MONGO env vars! Please inform Administrator immediately!');
-    process.exit(1);
-  }
-
-  if (
-    exists(process.env.GOOGLE_CLIENT_ID_DEV)
-    && exists(process.env.GOOGLE_CLIENT_SECRET_DEV)
-    && exists(process.env.GOOGLE_REDIRECT_URI_DEV)
-  ) {
-    const googleObj = {
-      CLIENT_ID: process.env.GOOGLE_CLIENT_ID_DEV,
-      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET_DEV,
-      REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI_DEV,
-    };
-
-    resultObj.google = googleObj;
-  } else {
-    logger.error('Issue with GOOGLE env vars! Please inform Administrator immediately!');
-    process.exit(1);
-  }
-
-  const otherObj = {
-    HOST: process.env.HOST_DEV || 'http://127.0.0.1',
-    PORT: process.env.PORT_DEV || 3000,
-    CLIENT_URL: process.env.CLIENT_URL_DEV,
-    CLIENT_DOMAIN: process.env.CLIENT_DOMAIN_DEV,
+if (
+  exists(process.env.GOOGLE_CLIENT_ID)
+  && exists(process.env.GOOGLE_CLIENT_SECRET)
+  && exists(process.env.GOOGLE_REDIRECT_URI)
+) {
+  const googleObj = {
+    CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
   };
 
-  resultObj.other = otherObj;
-} else if (NODE_ENV === 'PROD') {
-  if (exists(process.env.MONGO_URI_PROD)) {
-    const mongoObj = { URI: process.env.MONGO_URI_PROD };
-    resultObj.mongo = mongoObj;
-  } else {
-    logger.error('Issue with MONGO env vars! Please inform Administrator immediately!');
-    process.exit(1);
-  }
+  resultObj.google = googleObj;
+} else {
+  logger.error('Issue with GOOGLE env vars! Please inform Administrator immediately!');
+  process.exit(1);
+}
 
-  if (
-    exists(process.env.GOOGLE_CLIENT_ID_PROD)
-    && exists(process.env.GOOGLE_CLIENT_SECRET_PROD)
-    && exists(process.env.GOOGLE_REDIRECT_URI_PROD)
-  ) {
-    const googleObj = {
-      CLIENT_ID: process.env.GOOGLE_CLIENT_ID_PROD,
-      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET_PROD,
-      REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI_PROD,
-    };
-
-    resultObj.google = googleObj;
-  } else {
-    logger.error('Issue with GOOGLE env vars! Please inform Administrator immediately!');
-    process.exit(1);
-  }
-
+if (
+  exists(process.env.HOST) &&
+  exists(process.env.PORT) &&
+  exists(process.env.CLIENT_URL) &&
+  exists(process.env.NODE_ENV)
+) {
   const otherObj = {
-    HOST: process.env.HOST_PROD || 'http://127.0.0.1',
-    PORT: process.env.PORT_PROD || process.env.PORT || 3000, // PORT env variable is automatically created by Heroku
-    CLIENT_URL: process.env.CLIENT_URL_PROD,
-    CLIENT_DOMAIN: process.env.CLIENT_DOMAIN_PROD,
+    HOST: process.env.HOST || 'http://127.0.0.1',
+    PORT: process.env.PORT || 3000,
+    CLIENT_URL: process.env.CLIENT_URL,
+    NODE_ENV: process.env.NODE_ENV
   };
-
+  
   resultObj.other = otherObj;
+} else {
+  logger.error('Issue with OTHER env vars! Please inform Administrator immediately!');
+  process.exit(1);
 }
 
 module.exports = resultObj;
